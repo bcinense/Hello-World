906bf797a7d1da1009dda334263f890834d2ab21
bcinense <46617761+bcinense@users.noreply.github.com>
2020-11-10 10:48:53 -1000

    Update server.js

--- Cinense_Brittany_Assignment1/server.js
+++ Cinense_Brittany_Assignment1/server.js
@@ -80,6 +80,7 @@ app.post("/process_form", function (request, response) {
 });
 
 function isNonNegInt(q, returnErrors = false) {
+  // Function from Lab12
   errors = []; // assume no errors at first
   if (Number(q) != q) errors.push("Not a number!"); // Check if string is a number value
   if (q < 0) errors.push("Negative value!"); // Check if it is non-negative
