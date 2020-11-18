a1e2a337e79c70ba56eff2b64df045061707981c
bcinense <46617761+bcinense@users.noreply.github.com>
2020-11-17 22:36:07 -1000

    Assignment2 update

--- Cinense_Brittany_Assignment2/server.js
+++ Cinense_Brittany_Assignment2/server.js
@@ -3,6 +3,8 @@ var myParser = require("body-parser");
 var data = require("./public/product_data.js");
 var app = express(); //initialize express
 var fs = require("fs");
+const filename = "./public/user_data.json";
+const shoppingCartFile = "./public/shopping_cart.json";
 
 var products = data.product_data;
 
@@ -11,11 +13,24 @@ app.use(myParser.urlencoded({ extended: true }));
 
 // Opening a server to listen to
 var server = app.listen(8080, () => {
-  console.log(products);
+  console.log("STARTING SERVER");
 });
+
+// Copied code from Lab 14 Ex1.js to write in the user_data.json file info of new user registering
+if (fs.existsSync(filename)) {
+  var stats = fs.statSync(filename);
+
+  var data = fs.readFileSync(filename, "utf-8"); // When the file is executed it will read the data in the filename
+  var users_reg_data = JSON.parse(data); // Parse data, which hold the contents of the objects
+}
+if (fs.existsSync(shoppingCartFile)) {
+  var shoppingCartData = fs.readFileSync(shoppingCartFile, "utf-8");
+  var shoppingCart = JSON.parse(shoppingCartData);
+}
 
 app.post("/process_form", function (request, response) {
   let POST = request.body;
+  console.log(POST);
   var isValid = true;
   // Go through each product and validate that it is a number higher than 0
   products.forEach(function (product) {
@@ -25,50 +40,96 @@ app.post("/process_form", function (request, response) {
     }
   });
   if (isValid) {
-    // If the input values are valid (quantity > 0), respond back with invoice
-    var subtotal = 0;
-    var invoiceRows = ""; // Created an empty string to concacnate future rows
-    var extendedPrice;
-    products.forEach(function (product) {
-      var quantity = 0;
-      if (POST[product.veggie].length > 0) {
-        // If input has a value, assign it to quantity
-        quantity = POST[product.veggie];
-      }
-      if (quantity > 0) {
-        // If quantity > 0, calculate extended price, and add to subtotal
-        extendedPrice = quantity * product.price;
-        subtotal += extendedPrice;
-        // Generate invoice rows
+    var shoppingCartValues = JSON.stringify(POST);
+    fs.writeFileSync(shoppingCartFile, shoppingCartValues);
 
-        invoiceRows += `
-          <tr>
-            <th style="text-align: center" width="43%">${product.veggie}</td>
-            <th style="text-align: center" width="11%">
-              ${quantity}
-            </td>
-            <th style="text-align: center" width="13%">$${product.price.toFixed(
-              2
-            )}</td>
-            <th style="text-align: center" width="54%">$${extendedPrice.toFixed(
-              2
-            )}</td>
-          </tr>
-        `;
-      }
-    });
-    // Calculate sales tax
-    var tax_rate = 0.0575;
-    var tax = tax_rate * subtotal;
-
-    // Calculate grand total
-    var total = tax + subtotal;
-    var contents = fs.readFileSync("./public/invoice_template.view", "utf8");
-    response.send(eval("`" + contents + "`")); // render template string with variables
+    // Direct user to register page in order to complete purchase
+    response.sendFile(__dirname + "/public/register.html");
   } else {
     // If any input is invalid, send response of "Sorry, invalid input"
     response.send(`<p>Sorry, invalid input</p>`);
   }
+});
+// Code taken from Lab14 Ex1.js to register new user in the user_data.json file
+
+app.post("/register_new", function (request, response) {
+  var username = request.body.username;
+  users_reg_data[username] = {}; //leave blank
+  users_reg_data[username].password = request.body.password;
+  users_reg_data[username].email = request.body.email;
+  // Grab both first and last name and put together for the object name
+  users_reg_data[
+    username
+  ].name = `${request.body.firstname} ${request.body.lastname}`;
+  // write updated object to user_reg_info
+  reg_info_str = JSON.stringify(users_reg_data);
+  // will read the new data, parse it and read the new user info
+  fs.writeFileSync(filename, reg_info_str);
+  {
+    // Send a response after registering with a link to complete purchase and go to invoice
+    response.send(
+      `Thank you ${request.body.username} registering!<br>Please <a href="/invoice">click here</a> to complete your purchase`
+    );
+  }
+});
+// Code taken from Lab14 Ex1.js to retrieve username and password from user_data.json file
+app.post("/login_user", function (request, response) {
+  if (typeof users_reg_data[request.body.username] != "undefined") {
+    if (
+      request.body.password == users_reg_data[request.body.username].password
+    ) {
+      response.send(
+        `Thank you for ${request.body.username} logging in<br>Please <a href="/invoice">click here</a> to complete your purchase`
+      );
+    } else {
+      response.send(
+        `Sorry! ${request.body.password} does not match what we have for you`
+      );
+    }
+  } else {
+    response.send(`Sorry! ${request.body.username} does not exist`);
+  }
+});
+app.get("/invoice", function (request, response) {
+  // FINAL INVOICE WITH SUCCESSFUL LOGIN
+  // If the input values are valid (quantity > 0), respond back with invoice
+  var subtotal = 0;
+  var invoiceRows = ""; // Created an empty string to concacnate future rows
+  var extendedPrice;
+  products.forEach(function (product) {
+    var quantity = 0;
+    if (shoppingCart[product.veggie].length > 0) {
+      // If input has a value, assign it to quantity
+      quantity = shoppingCart[product.veggie];
+    }
+    if (quantity > 0) {
+      // If quantity > 0, calculate extended price, and add to subtotal
+      extendedPrice = quantity * product.price;
+      subtotal += extendedPrice;
+      // Generate invoice rows
+      invoiceRows += `
+        <tr>
+          <th style="text-align: center" width="43%">${product.veggie}</td>
+          <th style="text-align: center" width="11%">
+            ${quantity}
+          </td>
+          <th style="text-align: center" width="13%">$${product.price.toFixed(
+            2
+          )}</td>
+          <th style="text-align: center" width="54%">$${extendedPrice.toFixed(
+            2
+          )}</td>
+        </tr>
+      `;
+    }
+  });
+  // Calculate sales tax
+  var tax_rate = 0.0575;
+  var tax = tax_rate * subtotal;
+  // Calculate grand total
+  var total = tax + subtotal;
+  var contents = fs.readFileSync("./public/invoice_template.view", "utf8");
+  response.send(eval("`" + contents + "`")); // render template string with variables
 });
 
 function isNonNegInt(q, returnErrors = false) {
--- Lab14/Ex1.js
+++ Lab14/Ex1.js
@@ -13,17 +13,49 @@ if (fs.existsSync(filename)) {
 
   var data = fs.readFileSync(filename, "utf-8"); // When the file is executed it will read the data in the filename
   var users_reg_data = JSON.parse(data); // Parse data, which hold the contents of the objects
-  // if user exists, get their password
-  if (typeof users_reg_data["dport"] != "undefined") {
-    console.log(users_reg_data["dport"]["password"]) == "xxx";
-  }
-} else {
-  console.log(`ERR: ${filename} does not exist!`);
 }
+// if user exists, get their password
+// if (typeof users_reg_data["dport"] != "undefined") {
+//   console.log(users_reg_data["dport"]["password"]) == "xxx";
+// }
 
 // console.log(users_reg_data["dport"]["password"]); // users_reg_data.dport.password to retrieve a particular password
 
 app.use(myParser.urlencoded({ extended: true }));
+
+app.get("/register", function (request, response) {
+  // Give a simple register form
+  str = `
+<body>
+<form action="" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
+<input type="email" name="email" size="40" placeholder="enter email"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+  response.send(str);
+});
+
+app.post("/register", function (request, response) {
+  // process a simple register form
+  // validate the reg info
+
+  // If all data is valid write to the user_datat_filename and send to invoice
+
+  // add an ex. new user reg info
+  username = request.body.username; // use this for assignment to replace newuser with what the user typed in
+  users_reg_data[username] = {}; //leave blank
+  users_reg_data[username].password = request.body.password;
+  users_reg_data[username].email = request.body.email;
+
+  // write updated object to user_reg_info
+  reg_info_str = JSON.stringify(users_reg_data);
+  // will read the new data, parse it and read the new user info
+  fs.writeFileSync(filename, reg_info_str);
+});
 
 app.get("/login", function (request, response) {
   // Give a simple login form
@@ -46,15 +78,17 @@ app.post("/login", function (request, response) {
 
 app.listen(8080, () => console.log(`listening on port 8080`));
 
-// if user exists, get their password
-if (typeof users_reg_data[request.body.username] != "undefined") {
-  if (request.body.password == users_reg_data[request.body.username].password) {
-    response.send(`Thank you for ${request.body.username} logging in`);
-  } else {
-    response.send(
-      `Hey! ${request.body.password} does not match what we have for you`
-    );
-  }
-} else {
-  response.send(`Hey! ${request.body.username} does not exist`);
-}
+// // if user exists, get their password
+// if (typeof users_reg_data[request.body.username] != "undefined") {
+//   if (request.body.password == users_reg_data[request.body.username].password) {
+//     response.send(`Thank you for ${request.body.username} logging in`);
+//   } else {
+//     response.send(
+//       `Hey! ${request.body.password} does not match what we have for you`
+//     );
+//   }
+// } else {
+//   response.send(`Hey! ${request.body.username} does not exist`);
+// }
+
+//validate email check on the server!!
