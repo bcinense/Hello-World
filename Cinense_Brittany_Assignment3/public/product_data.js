1dfedd44af0a9deed462777f0fc69ec64f7d45aa
bcinense <46617761+bcinense@users.noreply.github.com>
2020-11-24 00:52:06 -1000

    Assignment3 update

---
+++ Cinense_Brittany_Assignment3/public/product_data.js
@@ -0,0 +1,35 @@
+product_data = [
+  {
+    veggie: "Potatoes",
+    price: 0.78,
+    image: "./images/potatoe.png",
+  },
+  {
+    veggie: "Mushrooms",
+    price: 0.25,
+    image: "./images/mushroom.jpg",
+  },
+  {
+    veggie: "Carrots",
+    price: 0.16,
+    image: "./images/carrots.png",
+  },
+  {
+    veggie: "Onion",
+    price: 1.3,
+    image: "./images/onion.jpg",
+  },
+  {
+    veggie: "Tomatoes",
+    price: 0.75,
+    image: "./images/tomato.jpeg",
+  },
+  {
+    veggie: "Lettuce",
+    price: 1.57,
+    image: "./images/lettuce.jpeg",
+  },
+];
+if (typeof module != undefined) {
+  module.exports.product_data = product_data;
+}
