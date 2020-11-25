b4f7ab3030ca450cbdc170fb0344c56fd996897b
bcinense <46617761+bcinense@users.noreply.github.com>
2020-11-24 23:30:10 -1000

    Assignment3 update

--- Cinense_Brittany_Assignment3/server.js
+++ Cinense_Brittany_Assignment3/server.js
@@ -3,6 +3,9 @@ var myParser = require("body-parser"); // Run body-parser
 var data = require("./public/product_data.js"); // Import and run product_data.js from the public folder
 var app = express(); // Initialize express
 var fs = require("fs"); // Require the File System module
+const { request } = require("express");
+const userInfo = "./public/user_data.json"; // Re-name file path to use in one varibale to be called throughout the server
+const shoppingCartFile = "./public/shopping_cart.json";
 
 app.use(express.static("./public")); // Accessing data from public file
 app.use(myParser.urlencoded({ extended: true }));
@@ -10,4 +13,16 @@ app.use(myParser.urlencoded({ extended: true }));
 // Opening a server to listen to
 var server = app.listen(8080, () => {
   console.log("STARTING SERVER");
+});
+
+// Copied code from Lab 14 Ex1.js to write in the user_data.json file info of new user registering
+if (fs.existsSync(userInfo)) {
+  var stats = fs.statSync(userInfo);
+
+  var data = fs.readFileSync(userInfo, "utf-8"); // When the file is executed it will read the data in the filename
+  var users_reg_data = JSON.parse(data); // Parse data, which hold the contents of the objects
+}
+
+app.post("/login_or_reg_user", function (request, response) {
+  response.send("You did it!");
 });
