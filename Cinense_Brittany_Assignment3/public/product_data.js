10893e799f472fdfe8ad740ef6824567ae276634
bcinense <46617761+bcinense@users.noreply.github.com>
2020-12-12 21:11:26 -1000

    commit
    Assign correct name and price to added products

--- Cinense_Brittany_Assignment3/public/product_data.js
+++ Cinense_Brittany_Assignment3/public/product_data.js
@@ -23,42 +23,42 @@ product_data = [
 
   {
     veggie: "Roma Tomato",
-    price: 1.57,
+    price: 1.15,
     image: "./images/romatomato.png",
   },
   {
     veggie: "Yellow Tomatoes",
-    price: 1.57,
+    price: 1.05,
     image: "./images/yellowtomatoes.png",
   },
   {
     veggie: "Portobello Mushrooms",
-    price: 1.57,
+    price: 1.02,
     image: "./images/portobello.png",
   },
   {
     veggie: "Enoki Mushrooms",
-    price: 1.57,
+    price: 0.82,
     image: "./images/enoki.png",
   },
   {
     veggie: "Red Potatoes",
-    price: 1.57,
+    price: 1.75,
     image: "./images/redpotatoes.png",
   },
   {
     veggie: "Sweet Potatoes",
-    price: 1.57,
+    price: 1.58,
     image: "./images/purplepotatoes.png",
   },
   {
     veggie: "Red Onions",
-    price: 1.57,
+    price: 1.99,
     image: "./images/redonions.png",
   },
   {
     veggie: "White Onions",
-    price: 1.57,
+    price: 1.89,
     image: "./images/whiteonions.png",
   },
 ];
